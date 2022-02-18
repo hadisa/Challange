fc06825535c9bc35ce77510600b391aaf6079fe4
hadisa <afg.norozi@gmail.com>
2022-02-18 08:39:02 -0800

    fix challange

--- turing.js
+++ turing.js
@@ -689,6 +689,8 @@ function divisibleSumPairs(n, k, ar) {
     }
     return count;
 }
+
+
 //----------------------------------------------------
 // There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
 // Function Description
