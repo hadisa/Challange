9cbcec4de3f1f445dc25fe9a9d6dfb26e779fa49
hadisa <afg.norozi@gmail.com>
2022-03-07 00:04:24 -0800

    Add New Challange

--- challang.js
+++ challang.js
@@ -333,3 +333,40 @@ var arr = [[1, 2, 3],
 [4, 5, 6]
 [9, 8, 9]]
 console.log(diagonalDifference(arr))
+//============================================================================
+/**
+ * An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly steps steps, for every step it was noted if it was an uphill,U , or a downhill,D  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+Given the sequence of up and down steps during a hike, find and print the number of valleys walked through
+ *
+    Returns
+   int: the number of valleys traversed
+ * 
+ */
+   function countingValleys(path) {
+    // Write your code here
+    console.log('Steps', steps)
+    console.log('Path >',path)
+    var countValley=0;
+    var count=0;
+    var countUp=0;
+    var countDown=0;
+    for(var i=0; i< steps; i++){
+        
+        if(path.charAt(i)==='U'){
+            countUp++;
+             console.log('UP', countUp)
+            
+        }else if(path.charAt(i)==='D'){
+            countDown++;
+             console.log('Down', countDown)
+        }
+        countValley= (countUp- countDown);
+       
+    }
+    console.log('Up and Down', countValley)
+return count;
+}
+console.log('Hi', countingValleys(['UDDDUDUU']))
